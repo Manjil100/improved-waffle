ea2b8b9b2ca2b6cfc1ce52c15facf419861ed7e2
Manjil100 <manjilkhatiwoda0@gmail.com>
2026-03-29 22:31:41 +0000

    Check: different root name in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-console.log(process.env.NODE_ENV);
 export default defineConfig({
-  plugins: [react()], 
-  // If we are deploying to GitHub Pages, use the repo name. 
-  // Otherwise (like on Vercel), use the root.
-  base: process.env.NODE_ENV === 'production' && !process.env.VERCEL 
-    ? '/improved-waffle/' 
-    : '/',
+  plugins: [react()],
+  base: '/improved-waffle/',
 })
