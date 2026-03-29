cdb4477c7f7e911de188aad661658e7d9f6264c6
Manjil100 <manjilkhatiwoda0@gmail.com>
2026-03-29 22:37:23 +0000

    Condition for deployment in vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/improved-waffle/',
+   base: process.env.VERCEL ? '/' : '/improved-waffle/',
 })
+
+/* 
+
+Github deploy:
+export default defineConfig({
+  plugins: [react()],
+   base: '/improved-waffle/'
+})
+
+*/
