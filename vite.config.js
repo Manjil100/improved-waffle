102c2ea86c0f7022cb142003af59cdc074050d6d
Manjil100 <manjilkhatiwoda0@gmail.com>
2026-03-29 22:20:27 +0000

    VERCEL99

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+console.log(process.env.NODE_ENV);
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], 
   // If we are deploying to GitHub Pages, use the repo name. 
   // Otherwise (like on Vercel), use the root.
   base: process.env.NODE_ENV === 'production' && !process.env.VERCEL 
